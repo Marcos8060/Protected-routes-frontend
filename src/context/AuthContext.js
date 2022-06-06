0fd9f6aadb0c19464ba0c31393d9d2c5fd92189e
Marcos8060 <marcosgav80@gmail.com>
2022-06-06 23:41:58 +0300

    created an token update functionality

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, usEffect } from "react";
+import { createContext, useState, usEffect, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 import { useNavigate } from "react-router-dom";
 
@@ -14,6 +14,7 @@ export const AuthProvider = ({ children }) =>{
     // call back ensures the state is called once in the initial render.
     let [ authTokens, setAuthTokens] = useState(()=> localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')):(null))
     let [ user, setUser] = useState(()=> localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')):(null))
+    let [loading,setLoading] = useState(true)
 
 
     let loginUser = async(e) =>{
@@ -43,11 +44,43 @@ export const AuthProvider = ({ children }) =>{
         localStorage.removeItem('authTokens')
         history('/login')
     }
+
+    let updateToken = async()=>{
+        console.log('update token called');
+        let response = await  fetch('http://127.0.0.1:8000/api/token/refresh/',{
+            method: 'POST',
+            headers:{ 
+                'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify({ 'refresh':authTokens.refresh})
+        })
+        let data = await response.json();
+        if(response.status === 200){
+            setAuthTokens(data)
+            setUser(jwt_decode(data.access))
+            localStorage.setItem('authTokens', JSON.stringify(data))
+        }else{
+            logoutUser();
+        } 
+
+    }
+
     let contextData = {
         user: user,
         loginUser: loginUser,
         logoutUser: logoutUser
     }
+
+    useEffect(() =>{
+
+        let fourMinutes = 1000 * 60 * 4
+        let interval = setInterval(() =>{
+            if(authTokens){
+                updateToken();
+            }
+        },fourMinutes)
+        return () => clearInterval(interval)
+    },[authTokens, loading])
 
     return(
         <AuthContext.Provider value={contextData}>
